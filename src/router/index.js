4381e740dbdbd6e026a8e1f6e6859c59c9aa7b85
li <623071187@example.com>
2020-01-06 15:33:09 +0800

    侧边栏嵌套路由

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,13 @@ import login from '../views/login/login.vue'
 import index from '../views/index/index.vue'
 
 
+//嵌套路由
+import chart from '../views/index/chart/chart.vue'
+import user from '../views/index/user/user.vue'
+import question from '../views/index/question/question.vue'
+import enterprise from '../views/index/enterprise/enterprise.vue'
+import subject from '../views/index/subject/subject.vue'
+
 //注册
 Vue.use(VueRouter)
 
@@ -17,7 +24,29 @@ const router =new VueRouter({
         },
         {
             path:'/index',
-            component:index
+            component:index,
+            children:[
+                {
+                    path:'chart',
+                    component:chart,
+                },
+                {
+                    path:'user',
+                    component:user,
+                },
+                {
+                    path:'question',
+                    component:question,
+                },
+                {
+                    path:'enterprise',
+                    component:enterprise,
+                },
+                {
+                    path:'subject',
+                    component:subject,
+                }
+            ]
         }
     ]
 })
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -19,32 +19,32 @@
     <el-aside width="auto" class="left-box">
        <!-- <el-col :span="24"> -->
             <el-menu
-              default-active="2"
+              :default-active="$route.path"
               class="el-menu-vertical-demo"
               :collapse='kaiguan'
-              
+              router
             >
-              <el-menu-item index="1">
+              <el-menu-item index="/index/chart">
                 <i class="el-icon-pie-chart"></i>
                 <span slot="title">数据概览</span>
               </el-menu-item>
              
-              <el-menu-item index="2">
+              <el-menu-item index="/index/user">
                 <i class="el-icon-user"></i>
                 <span slot="title">用户列表</span>
               </el-menu-item>
              
-              <el-menu-item index="3">
+              <el-menu-item index="/index/question">
                 <i class="el-icon-edit-outline"></i>
                 <span slot="title">题库列表</span>
               </el-menu-item>
              
-              <el-menu-item index="4">
+              <el-menu-item index="/index/enterprise">
                 <i class="el-icon-notebook-2"></i>
                 <span slot="title">企业列表</span>
               </el-menu-item>
              
-              <el-menu-item index="5">
+              <el-menu-item index="/index/subject">
                 <i class="el-icon-office-building"></i>
                 <span slot="title">学科列表</span>
               </el-menu-item>
@@ -53,7 +53,9 @@
           <!-- </el-col> -->
     </el-aside>
     <!-- 右边盒子 -->
-    <el-main class="right-box">Main</el-main>
+    <el-main class="right-box">
+      <router-view></router-view>
+    </el-main>
   </el-container>
 </el-container>
 
